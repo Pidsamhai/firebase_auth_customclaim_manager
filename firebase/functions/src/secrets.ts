e692fd045221b2b760cdb2eb1d436e5104ec151c
mengxyz <meng348@gmail.com>
2021-12-26 02:43:40 +0700

    [fixed] emulator can't run when read functions config
    - fix eslint secrets_key camel case to secrets.key

--- firebase/functions/src/secrets.ts
+++ firebase/functions/src/secrets.ts
@@ -4,6 +4,6 @@ import * as functions from "firebase-functions";
 dotenv.config();
 
 const LOCAL = process.env.secrets_key;
-const FUNCTIONS = functions.config().env.secrets_key;
+const FUNCTIONS = functions.config().env?.secrets_key;
 const SECRETS_KEY: string = LOCAL || FUNCTIONS;
 export {SECRETS_KEY};
